7fc3bf03a84eb49481f8e6d7891718d00fe9b7c6
robin <robinvierich@gmail.com>
2022-12-03 15:57:55 -0500

    added input helper to open and read input files

---
+++ common/input_helper.py
@@ -0,0 +1,26 @@
+from enum import Enum
+
+class InputType(Enum):
+    SAMPLE_INPUT = 1,
+    REAL_INPUT = 2
+
+
+def read_input_lines(executing_file_path, input_type):
+
+    input_filename = ""
+
+    if input_type == InputType.SAMPLE_INPUT:
+        input_filename = "sample_input.txt"
+    elif input_type == InputType.REAL_INPUT:
+        input_filename = "input.txt"
+
+    import os
+
+    abspath = os.path.abspath(executing_file_path)
+    dname = os.path.dirname(abspath)
+
+    with open("{dir}/{filename}".format(dir = dname, filename = input_filename), 'r') as f:
+        return f.readlines()
+
+
+    
