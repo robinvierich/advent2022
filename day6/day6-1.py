6a29ffee108a91705d9cb08f288b4f0f48ca2171
robin <robinvierich@gmail.com>
2022-12-06 11:37:33 -0500

    day6

---
+++ day6/day6-1.py
@@ -0,0 +1,31 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from common.input_helper import InputType, read_input_lines
+
+
+def main():
+    lines = read_input_lines(__file__, InputType.REAL_INPUT)
+
+
+    for line in lines:
+
+        line_len = len(line)
+
+        for i in range(4, line_len):
+            chars = line[i-4:i]
+
+            all_different = len(set(chars)) == len(chars)
+
+            if all_different:
+                print("all different at index {i}".format(i = i))
+                break
+
+
+            
+
+
+
+    
+if __name__ == "__main__":
+    main()
